1f81867d25c56b54559ad8c73c0472d82c7ccc93
pranavp1618 <pranavpraveen268@gmail.com>
2025-06-24 22:12:28 +0530

    new changes

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-import numpy
+import numpy,pandas
+import sklearn.datasets
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+dataset = sklearn.datasets.load_breast_cancer()
+dataframe = pandas.DataFrame(dataset.data,columns=dataset.feature_names)
+#print(dataframe.head())
+dataframe['label']=dataset.target
+#print(dataframe.tail())
+#print(dataframe.shape) #rows and columns info
+#print(dataframe.info()) #more info about dataframe
+#print(dataframe.isnull().sum()) #to check nullvalues
+#print(dataframe.describe()) # for getting statistical info about dataset
+
+# M-malignant, B-benign
+dat1=dataframe.groupby('label').mean()
+print(dat1)
